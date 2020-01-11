082e04f4787a7d209a19e56cf195223972110ea8
Apollo <apollo@triangle.eecs.berkeley.edu>
2020-01-10 18:00:15 -0800

    modified BUILD and record_exporter.cc

--- record_exporter/record_exporter.cc
+++ record_exporter/record_exporter.cc
@@ -38,107 +38,6 @@ using ::apollo::cyber::record::RecordMessage;
 using apollo::cyber::common::GetFileName;
 
 
-using namespace std; 
-  
-// Define Infinite (Using INT_MAX caused overflow problems) 
-// Polygon related code copied from : https://www.geeksforgeeks.org/how-to-check-if-a-given-point-lies-inside-a-polygon/
-#define INF 10000 
-  
-struct Point 
-{ 
-    int x; 
-    int y; 
-}; 
-  
-// Given three colinear points p, q, r, the function checks if 
-// point q lies on line segment 'pr' 
-bool onSegment(Point p, Point q, Point r) 
-{ 
-    if (q.x <= max(p.x, r.x) && q.x >= min(p.x, r.x) && 
-            q.y <= max(p.y, r.y) && q.y >= min(p.y, r.y)) 
-        return true; 
-    return false; 
-} 
-  
-// To find orientation of ordered triplet (p, q, r). 
-// The function returns following values 
-// 0 --> p, q and r are colinear 
-// 1 --> Clockwise 
-// 2 --> Counterclockwise 
-int orientation(Point p, Point q, Point r) 
-{ 
-    int val = (q.y - p.y) * (r.x - q.x) - 
-              (q.x - p.x) * (r.y - q.y); 
-  
-    if (val == 0) return 0;  // colinear 
-    return (val > 0)? 1: 2; // clock or counterclock wise 
-} 
-  
-// The function that returns true if line segment 'p1q1' 
-// and 'p2q2' intersect. 
-bool doIntersect(Point p1, Point q1, Point p2, Point q2) 
-{ 
-    // Find the four orientations needed for general and 
-    // special cases 
-    int o1 = orientation(p1, q1, p2); 
-    int o2 = orientation(p1, q1, q2); 
-    int o3 = orientation(p2, q2, p1); 
-    int o4 = orientation(p2, q2, q1); 
-  
-    // General case 
-    if (o1 != o2 && o3 != o4) 
-        return true; 
-  
-    // Special Cases 
-    // p1, q1 and p2 are colinear and p2 lies on segment p1q1 
-    if (o1 == 0 && onSegment(p1, p2, q1)) return true; 
-  
-    // p1, q1 and p2 are colinear and q2 lies on segment p1q1 
-    if (o2 == 0 && onSegment(p1, q2, q1)) return true; 
-  
-    // p2, q2 and p1 are colinear and p1 lies on segment p2q2 
-    if (o3 == 0 && onSegment(p2, p1, q2)) return true; 
-  
-     // p2, q2 and q1 are colinear and q1 lies on segment p2q2 
-    if (o4 == 0 && onSegment(p2, q1, q2)) return true; 
-  
-    return false; // Doesn't fall in any of the above cases 
-} 
-  
-// Returns true if the point p lies inside the polygon[] with n vertices 
-bool isInside(Point polygon[], int n, Point p) 
-{ 
-    // There must be at least 3 vertices in polygon[] 
-    if (n < 3)  return false; 
-  
-    // Create a point for line segment from p to infinite 
-    Point extreme = {INF, p.y}; 
-  
-    // Count intersections of the above line with sides of polygon 
-    int count = 0, i = 0; 
-    do
-    { 
-        int next = (i+1)%n; 
-  
-        // Check if the line segment from 'p' to 'extreme' intersects 
-        // with the line segment from 'polygon[i]' to 'polygon[next]' 
-        if (doIntersect(polygon[i], polygon[next], p, extreme)) 
-        { 
-            // If the point 'p' is colinear with line segment 'i-next', 
-            // then check if it lies on segment. If it lies, return true, 
-            // otherwise false 
-            if (orientation(polygon[i], p, polygon[next]) == 0) 
-               return onSegment(polygon[i], p, polygon[next]); 
-  
-            count++; 
-        } 
-        i = next; 
-    } while (i != 0); 
-  
-    // Return true if count is odd, false otherwise 
-    return count&1;  // Same as (count%2 == 1) 
-} 
-
 void DisplayUsage(const std::string& binary) {
   std::cerr << "usage: " << binary << " <record_fiename>\n"
             << std::endl;
@@ -157,9 +56,12 @@ void ExportRecords(const std::string &readfile) {
   using namespace std;
   RecordReader reader(readfile);
   RecordMessage message;
+  // std::string file_name(readfile);
+  // std::string dir_name(readfile);
+  cout << "readfile: " << readfile << "\n";
   
-  auto pose_filename = "out/" + GetFileName(readfile) + "-ego.csv";
-  auto obst1_filename = "out/" + GetFileName(readfile) + "-pedestrian.csv";
+  auto pose_filename = "out/" + readfile + "-ego.csv";
+  auto obst1_filename = "out/" + readfile + "-pedestrian.csv";
 
   std::ofstream pose_file(pose_filename, std::ios::out);
   std::ofstream obst1_file(obst1_filename, std::ios::out);
@@ -193,9 +95,6 @@ void ExportRecords(const std::string &readfile) {
   uint64_t i = 0;
   std::map<int, std::shared_ptr<ofstream>> obs_map;
 
-  Point polygon1[] = {{586610, 4208097}, {586615, 4208098}, {586618, 4208090}, {586613, 4208089}}; 
-  int n = (int) (sizeof(polygon1)/sizeof(polygon1[0])); 
-
   for (i = 0; reader.ReadMessage(&message); ++i) {
 
     if (message.channel_name.compare("/apollo/perception/obstacles") == 0) {
@@ -213,8 +112,6 @@ void ExportRecords(const std::string &readfile) {
         const auto& obstacles = msg.perception_obstacle();
         
         if (obstacles.size() >= 1){
-          cout << "obstacle size > 1 \n";
-
           int j = 0;
 
           for (j = 0; j < obstacles.size(); ++j){
@@ -225,11 +122,11 @@ void ExportRecords(const std::string &readfile) {
             const auto& theta = obst.theta();
             const auto& acceleration = obst.acceleration();
             const auto& type = obst.type();
-
-            cout << "writing to the object #: " << to_string(j) << "\n";
             
-            Point p = {(int) position.x(), (int) position.y()}; 
-            if(isInside(polygon1, n, p)){
+            bool x_condition = (586609.83<=position.x()) && (position.x()<=586617.96); 
+            bool y_condition = (4208089.37<=position.y()) && (position.y()<=4208098);
+
+            if( x_condition && y_condition ){
               obst1_file << std::setprecision(6) << std::fixed 
               << msg.header().timestamp_sec() << ";"
               << id << ";" << theta <<";" << type <<";"
@@ -240,7 +137,7 @@ void ExportRecords(const std::string &readfile) {
             }
           }
 
-          cout << "all obstacles looped through\n";
+          // cout << "all obstacles looped through\n";
         }
 
       } else {
@@ -287,18 +184,13 @@ void ExportRecords(const std::string &readfile) {
     }
   }
 
-  // for (int i= 0; i <25 ; ++i){
-  //   files[i]->close();
-  // }
-  // cout << "files all closed\n";  
 }
-// std::cerr << "MSG totalcount: " << i << std::endl;
 
 
 using namespace std;
 int main(int argc, char *argv[]) {
   std::string binary = GetFileName(std::string(argv[0]));
-  
+
   if (argc < 2) {
     DisplayUsage(binary);
     return -1;
@@ -307,37 +199,13 @@ int main(int argc, char *argv[]) {
   apollo::cyber::Init(argv[0]);
   
   sleep(1);
-  std::string file_name(argv[1]);
+  // std::string file_name(argv[1]);
+  std::string file_path = argv[1];
   
   // TODO setup logger to use ADEBUG/AINFO instead of std::cerr
 
-  std::cerr << "Reading " << file_name << std::endl;
-  ExportRecords(file_name);
-
-  // Point polygon1[] = {{0, 0}, {10, 0}, {10, 10}, {0, 10}}; 
-  // int n = (int) (sizeof(polygon1)/sizeof(polygon1[0])); 
-
-  // Point p = {20, 20}; 
-  // isInside(polygon1, n, p)? cout << "Yes \n": cout << "No \n"; 
-
-  // p = {5, 5}; 
-  // isInside(polygon1, n, p)? cout << "Yes \n": cout << "No \n"; 
-
-  // Point polygon2[] = {{0, 0}, {5, 5}, {5, 0}}; 
-  // p = {3, 3}; 
-  // n = (int) (sizeof(polygon2)/sizeof(polygon2[0])); 
-  // isInside(polygon2, n, p)? cout << "Yes \n": cout << "No \n"; 
-
-  // p = {5, 1}; 
-  // isInside(polygon2, n, p)? cout << "Yes \n": cout << "No \n"; 
-
-  // p = {8, 1}; 
-  // isInside(polygon2, n, p)? cout << "Yes \n": cout << "No \n"; 
-
-  // Point polygon3[] =  {{0, 0}, {10, 0}, {10, 10}, {0, 10}}; 
-  // p = {-1,10}; 
-  // n = (int) (sizeof(polygon3)/sizeof(polygon3[0])); 
-  // isInside(polygon3, n, p)? cout << "Yes \n": cout << "No \n"; 
+  std::cerr << "Reading " << file_path << std::endl;
+  ExportRecords(file_path);
 
   return 0;
 }
